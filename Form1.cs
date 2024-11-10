1ad2f53bc572deac5045832b601182f37c560dfe
kaganarsln <kaan.arslangorurr@gmail.com>
2024-11-10 19:28:00 +0300

    CSHARP_ArmstrongSayiBulma
    C# Windows Forms ile Geliştirilmiştir

---
+++ Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ARMSTRONG_SAYISI_BULMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Armstrongsayi(int sayi) 
+        {
+            int girilensayi = sayi;
+            int toplam = 0;
+            int basamaksayisi = sayi.ToString().Length;
+            
+            for(int i=0; i< basamaksayisi; i++) 
+            {
+                int basamak = sayi % 10;
+
+                int kuvvetal = 1;
+                for(int j=0; j<basamaksayisi; j++) 
+                {
+                    kuvvetal *= basamak;
+                
+                }
+                toplam = toplam + kuvvetal;
+                sayi /= 10;
+
+            
+            }
+            if (girilensayi == toplam) 
+            {
+                listBox1.Items.Add(girilensayi + "= Girdiğiniz Sayı Bir Armstrong Sayısıdır.");
+            
+            }
+            else 
+            {
+                listBox1.Items.Add(girilensayi + "= Girdiğiniz Sayı Bir Armstrong Sayısı Değildir.");
+            
+            }
+        
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi = Convert.ToInt32(textBox1.Text);
+            Armstrongsayi(sayi);
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("ARMSTRONG SAYISI BULMA")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ARMSTRONG SAYISI BULMA")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("25d623ba-33f1-4ac1-b164-4d34733c18f0")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
